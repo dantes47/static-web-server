a37c6d876b500af0c4baa5f2ddfc5e4c3f298819
Danny <dantes817@gmail.com>
2020-06-07 22:20:19 +0200

    were added json/query encoding

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Testing.."))
+		names := r.URL.Query()["name"]
+		var name string
+		if len(names) == 1 {
+			name = names[0]
+		}
+		// w.Write([]byte("Say 'Hi' to " + name))
+		m := map[string]string{"name": name}
+		enc := json.NewEncoder(w)
+		enc.Encode(m)
 	})
 
 	err := http.ListenAndServe(":3000", nil)
