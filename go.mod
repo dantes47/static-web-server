f2c5a615649f83417f7d53d1c5edd53c9fdee88a
Danny <dantes817@gmail.com>
2020-06-07 22:02:45 +0200

    Initial commit/were created stat. server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dantes47/static-web-server
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Testing.."))
+	})
+
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
